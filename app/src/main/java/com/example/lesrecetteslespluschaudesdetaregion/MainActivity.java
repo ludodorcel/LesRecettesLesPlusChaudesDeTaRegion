1c16a3a3166dbe6a6172e183befc5aef1a19f043
Ludodorcel <merlier@et.esiea.fr>
2020-05-22 15:59:13 +0200

    modification de l'interface

--- app/src/main/java/com/example/lesrecetteslespluschaudesdetaregion/MainActivity.java
+++ app/src/main/java/com/example/lesrecetteslespluschaudesdetaregion/MainActivity.java
@@ -10,7 +10,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
